f127ac571ac0401ea35a95bb5ed8a160c19bb569
Mohamad Yaqoob Wajdi <yaqoob.wajdi@gmail.com>
2025-06-05 12:37:16 -0400

    changes to navbar and index.html

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx}"],
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx}"
+    ],
     mode: "jit",
     theme: {
       extend: {
@@ -16,7 +19,7 @@ module.exports = {
           card: "0px 35px 120px -15px #211e35",
         },
         screens: {
-          xs: "450px",
+          xs: "450px",  
         },
         backgroundImage: {
           "hero-pattern": "url('/src/assets/herobg.png')",
